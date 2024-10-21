6e824aae128ce0c945815fbc6a3c749889bc5205
Xandersousa <alexandre.r.sousa@gmail.com>
2024-10-20 21:12:00 -0300

    Modificações no projeto template Material-UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // Se existir
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Meu Projeto Material-UI Modificado</h1>
+        <p>Esta é uma alteração para a atividade da Faculdade Descomplica.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
